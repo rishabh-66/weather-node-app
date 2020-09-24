fcb7e503ef9afa1429b1b6b72cede68716d31111
rishabh-66 <58128726+rishabh-66@users.noreply.github.com>
2020-09-24 14:30:21 +0530

    Update geocode.js

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -4,7 +4,7 @@ const geocode = (address, callback) => {
   const url =
     "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
     encodeURIComponent(address) +
-    ".json?access_token=pk.eyJ1IjoicmpyaSIsImEiOiJja2V1eW9mdnozb2V3MnJwaW5mYjJrZDgyIn0.VedoXXpD0pWbMPZO0D5Ktw&limit=1";
+    ".json?access_token=sk.eyJ1IjoicmpyaSIsImEiOiJja2V1e.fedoXXpD0pWbMPZO0D5Ktw&limit=1";
   request({ url, json: true }, (error, {body}) => {
     if (error) {
       callback("Unable to connect", undefined);
